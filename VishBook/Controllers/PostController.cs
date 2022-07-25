831c6ba7c0ee0ec610d91d327811abcfe54e3ae4
Vishal Trivedi <vishaltrivedi92@gmail.com>
2022-07-25 15:47:07 -0500

    Post Repository Finished Index view up and running

---
+++ VishBook/Controllers/PostController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using VishBook.Models;
+using VishBook.Repositories;
+
+namespace VishBook.Controllers
+
+{
+    public class PostController : Controller
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly IUserProfileRepository _userProfileRepository;
+    public PostController(IPostRepository postRepository, IUserProfileRepository userProfileRepository)
+    {
+        _postRepository = postRepository;
+        _userProfileRepository = userProfileRepository;
+    }
+        private int GetCurrentUserId()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return int.Parse(id);
+        }
+
+        public ActionResult Index()
+        {
+            int ownerId = GetCurrentUserId();
+
+            List<Post> posts = _postRepository.GetPostByUserId(ownerId);
+
+            return View(posts);
+
+        }
+
+    }
+}
---
+++ VishBook/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VishBook.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
